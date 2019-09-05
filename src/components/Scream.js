5b24a75632545a51679f4bc88a455817ab60d887
Jonny Ramen <jpper@umich.edu>
2019-09-05 16:28:09 +0900

    Implemented dayjs to display how long it's been since the scream

--- src/components/Scream.js
+++ src/components/Scream.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import withStyles from "@material-ui/core/styles/withStyles";
-import Link from "react-router-dom/Link";
+import { Link } from "react-router-dom";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
 
 // MUI Stuff
 import Card from "@material-ui/core/Card";
@@ -14,16 +16,17 @@ const styles = {
     marginBottom: 20
   },
   image: {
-      minWidth: 200,
+    minWidth: 200
   },
   content: {
-      padding: 25,
-      objectFit: 'cover'
+    padding: 25,
+    objectFit: "cover"
   }
 };
 
 class Scream extends Component {
   render() {
+    dayjs.extend(relativeTime);
     const {
       classes,
       scream: {
@@ -38,8 +41,12 @@ class Scream extends Component {
     } = this.props;
     return (
       <Card className={classes.card}>
-        <CardMedia image={userImage} title="Profile Image" className={classes.image} />
-        <CardContent class={classes.content}>
+        <CardMedia
+          image={userImage}
+          title="Profile Image"
+          className={classes.image}
+        />
+        <CardContent className={classes.content}>
           <Typography
             variant="h5"
             component={Link}
@@ -49,7 +56,7 @@ class Scream extends Component {
             {userHandle}
           </Typography>
           <Typography variant="body2" color="textSecondary">
-            {createdAt}
+            {dayjs(createdAt).fromNow()}
           </Typography>
           <Typography variant="body1">{body}</Typography>
         </CardContent>
