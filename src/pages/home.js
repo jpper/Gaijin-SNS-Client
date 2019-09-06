4d47ee01d95bab8da00992d538ec0eae08d286f9
Jonathan Perry <jpper@umich.edu>
2019-09-07 07:56:04 +0900

    Finished profile section but now all display disappears

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import Grid from "@material-ui/core/Grid";
 
 import Scream from "../components/Scream";
+import Profile from "../components/Profile";
 
 class home extends Component {
   state = {
@@ -21,7 +22,9 @@ class home extends Component {
   }
   render() {
     let recentScreamsMarkup = this.state.screams ? (
-      this.state.screams.map((scream) => <Scream key={scream.screamId} scream={scream} />)
+      this.state.screams.map(scream => (
+        <Scream key={scream.screamId} scream={scream} />
+      ))
     ) : (
       <p>Loading...</p>
     );
@@ -31,7 +34,7 @@ class home extends Component {
           {recentScreamsMarkup}
         </Grid>
         <Grid item sm={4} xs={12}>
-          <p>Profile!</p>
+          <Profile />
         </Grid>
       </Grid>
     );
