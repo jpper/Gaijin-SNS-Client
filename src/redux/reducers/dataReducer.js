985200d35192a89403ef6b71ba564960f0dce2f6
Jonathan Perry <jpper@umich.edu>
2019-09-07 20:31:20 +0900

    Implemented dialog details (like and comments displaying on scream dialog)

--- src/redux/reducers/dataReducer.js
+++ src/redux/reducers/dataReducer.js
@@ -30,13 +30,16 @@ export default function(state = initialState, action) {
       return {
         ...state,
         scream: action.payload
-      }
+      };
     case LIKE_SCREAM:
     case UNLIKE_SCREAM:
       let index = state.screams.findIndex(
         scream => scream.screamId === action.payload.screamId
       );
       state.screams[index] = action.payload;
+      if (state.scream.screamId === action.payload.screamId) {
+        state.scream = action.payload;
+      }
       return {
         ...state
       };
